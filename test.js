89d4b0dbfaaa6629fba67f2de304a9eebba40d60
Dmytro Sherstiuk <sherdg@gmail.com>
2022-12-27 20:22:47 +0200

    JS again

--- test.js
+++ test.js
@@ -2,5 +2,34 @@ let str = "Привіт";
 let str2 = 'Одинарні лапки також дозволяються';
 let phrase = `так можна вставляти ${str2}`;
 
-console.log(str);
-console.log(phrase);
+// console.log(str);
+// console.log(phrase);
+
+
+// typeof undefined // "undefined"
+
+// console.log(typeof 4) // "number"
+
+// console.log(typeof 10n) // "bigint"
+
+var check = false;
+// console.log(typeof check) // "boolean"
+
+// // typeof "foo" // "string"
+
+// // typeof Symbol("id") // "symbol"
+
+// // typeof Math // "object"  (1)
+
+// console.log(typeof null) // "object"  (2)
+
+// typeof alert // "function"  (3)
+
+var array = ['Name', 'Test', 'Check', 4, [1,2,2]]
+// console.log(array[0]);
+
+var json = {
+    name: "Test",
+    age: 42
+}
+console.log(json);
