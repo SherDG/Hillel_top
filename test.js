a6884e1a16f6dd2dfa1614f73f82ef73ce9f374d
Dmytro Sherstiuk <sherdg@gmail.com>
2022-12-27 21:00:59 +0200

    JS test

--- test.js
+++ test.js
@@ -31,5 +31,36 @@ var array = ['Name', 'Test', 'Check', 4, [1,2,2]]
 var json = {
     name: "Test",
     age: 42
-}
-console.log(json);
+};
+// console.log(json);
+
+let s = 'мій_' + 'рядок' + 2;
+// console.log(s);
+
+let s1 = '1' + 2;
+// console.log(s1);
+
+let x = 2
+x--;
+// console.log(x);
+// let year = 2015
+// let additionalCondition = 2
+
+// if (year == 2015 || additionalCondition === 1 && year == 2012 || additionalCondition === 2) {
+//     console.log( "Це правильно!" );
+//     console.log( "Ви такий розумний!" );
+//     if(additionalCondition == 2){
+//         console.log( "Це правильно2!" );
+//         console.log( "Ви такий розумний2!" );
+//     }
+//   }
+// else if(year == 2022){
+//     console.log( "Це точно!" );
+// }
+// else{
+//     console.log( "Спробуйте ще!" );
+// };
+
+for (let i = 0; i < 3; i++) { // показується 0, далі 1, потім 2
+    console.log( "Спробуйте ще !" + i );
+  }
