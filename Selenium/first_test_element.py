b439438afa67aa5118046872b0ee348edfb0ed4f
Dmytro Sherstiuk <sherdg@gmail.com>
2023-01-24 19:14:15 +0200

    Selenium

---
+++ Selenium/first_test_element.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+absolute_path = os.path.dirname(__file__)
+relative_path = "/"
+full_path = os.path.join(absolute_path, relative_path)
+# print(f'Path: {full_path}')
+# print('Path:'+ full_path)
+
+options = Options()
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service(full_path+'chromedriver'), options=options)
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://" + user + ":" + password + "@" + "qauto2.forstudy.space/")
+# driver.get("https://qauto2.forstudy.space/")
+
+time.sleep(3)  # sleep for 3 sec
+
+
+def is_element_present(self, how, what):
+    try:
+        self.driver.find_element(by=how, value=what)
+    except NoSuchElementException as e:
+        return False
+    return True
+
+
+# try:
+#     element = driver.find_element(By.ID, "contactsSection1")
+# except NoSuchElementException:
+#     print("No element found")
+
+element = driver.find_element(By.ID, 'contactsSection')  # this element is visible
+if element.is_displayed():
+    print("Element found")
+    not_found = True
+else:
+    print("Element not found")
+    not_found = False
+
+assert not_found
+
+driver.close()
---
+++ Selenium/first_test_waits.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+options.add_argument("--no-sandbox")
+
+
+# START DRIVER
+
+driver = webdriver.Chrome(service=Service('/home/dima/Завантаження/Hillel/chromedriver'), options=options)
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+#CLICK by clickable
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Sign In')]"))).click()
+
+
+#GET ATTRIBUTE
+signIn = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(), 'Sign In')]"))).get_attribute("formcontrolname")
+# signIn = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.ID, "signinEmail"))).get_attribute("formcontrolname")
+# print(signIn)
+
+#CLICK be presense
+# WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Sign In')]"))).click()
+
+elem = driver.find_element(By.XPATH, "//button[contains(text(), 'Sign In')]")
+elem.click()
+
+
+# goButton = driver.find_element(By.ID, "submit")
+# goButton.click()
+
+assert "No results found." not in driver.page_source
+driver.close()
